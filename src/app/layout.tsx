1e0135cb936dc0ec0c26d6d0260fbbb21aa0ae36
Yuvraj Harsh <yuvrajharsh04@gmail.com>
2024-11-27 17:23:41 +0530

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,28 @@
+import localFont from "next/font/local";
+import { PrismicPreview } from "@prismicio/next";
+import { repositoryName } from "@/prismicio";
+import "./app.css";
+import Header from "@/components/Header";
+
+const alpino = localFont({
+  src: "../../public/fonts/Alpino-Variable.woff2",
+  display: "swap",
+  weight: "100 900",
+  variable: "--font-alpino",
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={alpino.variable}>
+      <body className="overflow-x-hidden bg-yellow-300">
+        <Header />
+        <main>{children}</main>
+      </body>
+      <PrismicPreview repositoryName={repositoryName} />
+    </html>
+  );
+}
